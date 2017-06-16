841cf87a98a259194182e3c7e0c53aa4d2d7ebe0
Joshua Fogg <joshuafoggnz@gmail.com>
2017-06-16 17:29:34 +1200

    halfway through Q3

--- eloquent.js
+++ eloquent.js
@@ -32,3 +32,18 @@ console.log(isEven(-1));
 // → ??
 
 //3. Bean Counting:
+function countBs(string){
+  var counter=0;
+  for (var i = 0; i <= string.length; i++){
+  if (string.charAt(i)==="B")
+  	counter+=1;
+  }
+  return counter;
+}
+
+// Your code here.
+
+console.log(countBs("BBC"));
+// → 2
+//console.log(countChar("kakkerlak", "k"));
+// → 4
